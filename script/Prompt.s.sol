6a97990c6f91700fb6141d507cd6b9531ef1506f
hadzija7 <hadzijasale@gmail.com>
2024-04-18 12:00:08 +0200

    tutorial setup

--- script/Prompt.s.sol
+++ script/Prompt.s.sol
@@ -2,17 +2,11 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {Prompt} from "../src/Prompt.sol";
-import {OraSepoliaAddresses} from "../test/OraSepoliaAddresses.t.sol";
-import {AIOracle} from "../src/AIOracle.sol";
 
-contract PromptScript is Script, OraSepoliaAddresses {
+contract PromptScript is Script {
     function setUp() public {}
 
     function run() public {
-        uint privateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(privateKey);
-        new Prompt(AIOracle(OAO_PROXY));
-        vm.stopBroadcast();
+
     }
 }
