9fb9080b455f71098374351d9221fad8273d732d
hadzija7 <hadzijasale@gmail.com>
2024-04-18 14:28:49 +0200

    minor change

--- script/Prompt.s.sol
+++ script/Prompt.s.sol
@@ -1,12 +1,10 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Script, console2} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 
 contract PromptScript is Script {
     function setUp() public {}
 
-    function run() public {
-
-    }
+    function run() public {}
 }
